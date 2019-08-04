329e34332238b8a8ba5805639e2d2441fa650a49
Alice <alice.leon10@gmail.com>
2019-08-04 22:12:25 +0200

    Ajout de la fonction supprimer pour les factures. Manque rechargement de la liste...

---
+++ src/components/Buttons/ButtonDelete.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="button-delete">
+    <button class="btn-lg btn-danger float-right">
+      <font-awesome-icon icon="trash" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonDelete"
+};
+</script>
