9662ecdb74b66689fcd421ef2387d8f1a6e632fe
Alice <alice.leon10@gmail.com>
2019-08-05 15:33:46 +0200

    again

--- src/components/Buttons/ButtonDelete.vue
+++ src/components/Buttons/ButtonDelete.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="button-delete">
-<<<<<<< HEAD
     <button class="btn-lg btn-danger" @click="isClicked">
-=======
-    <button class="btn-lg btn-danger" @click="$emit('clicked')">
->>>>>>> 38a9bab5c2d29eabef7644b77875a8039c12c494
       <font-awesome-icon icon="trash" />
     </button>
   </div>
