40ad8e2300258bdda071259a4e9bf2736accca6c
Alice <alice.leon10@gmail.com>
2019-08-08 15:06:37 +0200

    Refacto de la liste, succès pour supplier O_O

---
+++ src/components/List.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="list">
+    <table class="table">
+      <thead>
+        <th v-for="(column, index) in columns" :key="index">{{column}}</th>
+        <th scope="col" class="text-center align-middle">Modifier</th>
+        <th scope="col" class="text-center align-middle">Supprimer</th>
+        <th scope="col" class="text-center align-middle">Voir plus</th>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in list" :key="index">
+          <td v-for="(type, index) in types" :key="index">
+            <div v-for="(element, name, index) in item" :key="index">
+              <div v-if="name == type">{{element}}</div>
+            </div>
+          </td>
+          <td class="text-center align-middle">
+            <ButtonEdit @clicked="$router.push(`fournisseurs/modifier/${list[index].id}`)" />
+          </td>
+          <td class="text-center align-middle">
+            <ButtonDelete @clicked="deleteSupplier(list[index].id)" />
+          </td>
+          <td class="text-center align-middle">
+            <ButtonMore @clicked="$router.push(`fournisseurs/${list[index].id}`)" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import ButtonEdit from "../components/Buttons/ButtonEdit";
+import ButtonMore from "../components/Buttons/ButtonMore";
+import ButtonDelete from "../components/Buttons/ButtonDelete";
+
+export default {
+  name: "List",
+  components: { ButtonEdit, ButtonDelete, ButtonMore },
+  props: {
+    columns: {
+      type: Array,
+      required: true
+    },
+    list: {
+      type: Array,
+      required: true
+    },
+    types: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      newList: []
+    };
+  }
+  /* methods: {
+    displayList: function(list) {
+      for (type in types) {
+        console.log(type);
+        for (item in list) {
+          if (item.key == type) {
+            newList.push(item);
+            console.log(item);
+          }
+        }
+      }
+    }
+  },
+  created() {
+    console.log(this.list);
+    console.log(this.types);
+  } */
+};
+</script>
--- src/components/SuppliersList.vue
+++ src/components/SuppliersList.vue
@@ -1,50 +1,22 @@
 <template>
   <div id="suppliers-list">
-    <table class="table">
-      <thead>
-        <th scope="col">Entreprise</th>
-        <th scope="col">Adresse</th>
-        <th scope="col">Code Postal</th>
-        <th scope="col">Ville</th>
-        <th scope="col">Pays</th>
-        <th scope="col" class="text-center align-middle">Modifier</th>
-        <th scope="col" class="text-center align-middle">Supprimer</th>
-        <th scope="col" class="text-center align-middle">Voir plus</th>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in list" :key="index">
-          <td>{{list[index].company}}</td>
-          <td>{{list[index].adress}}</td>
-          <td>{{list[index].postcode}}</td>
-          <td>{{list[index].city}}</td>
-          <td>{{list[index].country}}</td>
-          <td class="text-center align-middle">
-            <ButtonEdit @clicked="$router.push(`fournisseurs/modifier/${list[index].id}`)" />
-          </td>
-          <td class="text-center align-middle">
-            <ButtonDelete @clicked="deleteSupplier(list[index].id)" />
-          </td>
-          <td class="text-center align-middle">
-            <ButtonMore @clicked="$router.push(`fournisseurs/${list[index].id}`)" />
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="list.length > 0">
+      <List :columns="columns" :list="list" :types="types" />
+    </div>
   </div>
 </template>
 
 <script>
-import ButtonEdit from "../components/Buttons/ButtonEdit";
-import ButtonMore from "../components/Buttons/ButtonMore";
-import ButtonDelete from "../components/Buttons/ButtonDelete";
+import List from "./List";
 
 export default {
   name: "SuppliersList",
-  components: { ButtonEdit, ButtonDelete, ButtonMore },
+  components: { List },
   data() {
     return {
       list: [],
-      button: ""
+      columns: ["Entreprise", "Adresse", "Code Postal", "Ville", "Pays"],
+      types: ["company", "adress", "postcode", "city", "country"]
     };
   },
   methods: {
